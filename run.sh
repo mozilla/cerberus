95d0fa0e15ac9e09246dd75a483a0e2ba790303c
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-09-16 14:10:53 +0000

    Remove run.sh wrapper.

--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
+sudo apt-get -qq update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install nodejs npm python-numpy python-opencv python-matplotlib
+
+pushd .
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 git pull
 (cd exporter; npm install) &&
 rm -rf ./histograms &&
+
 wget https://raw.githubusercontent.com/mozilla/gecko-dev/master/toolkit/components/telemetry/Histograms.json &&
 nodejs exporter/export.js &&
 python alert/alert.py &&
 python alert/post.py
 
-cd -
+popd
