fea3e36d11e08787603cb5e3e00a581bb5c9b25d
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-11-06 20:24:17 +0000

    Overwrite previous Histograms.json file.

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ git pull
 (cd exporter; npm install) &&
 rm -rf ./histograms &&
 
-wget https://raw.githubusercontent.com/mozilla/gecko-dev/master/toolkit/components/telemetry/Histograms.json &&
+wget https://raw.githubusercontent.com/mozilla/gecko-dev/master/toolkit/components/telemetry/Histograms.json -O Histograms.json &&
 nodejs exporter/export.js &&
 python alert/alert.py &&
 python alert/post.py
